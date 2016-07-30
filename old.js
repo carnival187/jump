32674bc2014683acc0d94d0de2f49ffe2810aaa1
carnival187 <carnival187@outlook.fr>
2016-07-30 16:14:45 +0200

    move sides

--- old.js
+++ old.js
@@ -4,46 +4,76 @@ let makeMat = function(x, y, value = 0){
 	for(let i = 0; i < x; i++){
 		mat[i] = [];
 		for(let j = 0; j < y; j++){
-			mat[i][j] = value;
+			if(i == 0 || j == 0 || i == x-1 || j == y-1 || j%5 == 0 && i < x/4){
+				mat[i][j] = 1;
+			}else{
+				mat[i][j] = value;
+			}
 		}
 	}
 	return mat;
 };
-let timers = {};
-let map = makeMat(20, 30);
-let topJump = 10;
-let p = {x : 0, y : 5};
-let down = 0;
-let jumping = function(){
-	timers.jump = setTimeout(jumping, speed);
-};
-document.addEventListener('keydown', function(){
-	down = Date.now();
-});
-document.addEventListener('keyup', function(){
-	r = Math.round((Date.now() - down) / 20);
-	topJump = (r <= 10) ? r : 10;
-	console.log('top : ' + topJump);
-});
-let 
-/****************************************************/
-let div = document.createElement('div');
-document.body.appendChild(div);
-/****************************************************/
-let display = function(m){
-	let s = '';
-	for(let i = m.length-1; i >= 0 ; i--){
-		for(let j = 0; j < m[i].length; j++){
-			if( i == p.x && j == p.y){
-				s += 'X';
-			}else{
-				s += ' ';
-				s += m[i][j];
-				s += ' ';
+let g = {
+	canvas : document.createElement('div'),
+	p : {
+		x: 6,
+		y: 1,
+		move : function(direction){
+			switch(direction){
+				case "left":
+					if(this.x > 0) this.x--;
+				break;
+				case "right":
+					if(true) this.x++;
+				break;
 			}
+		},
+	},
+	map : makeMat(30, 400),
+	screen : {x: 0, large: 50},
+
+	init(){
+		document.body.appendChild(this.canvas);
+		document.addEventListener('keydown', (e)=> this.keypress(e));
+		this.apply();
+	},
+	apply : function(){
+		this.mapping();
+		this.display();
+	},
+	keypress : function(e){
+		switch(e.keyCode){
+			case 37:
+				if(this.screen.x > 0) this.screen.x--;
+				this.p.move("left");
+			break;
+			case 39:
+				this.screen.x++;
+				this.p.move("right");
+			break;
+			default:
 		}
-		s += '<br>';
+		this.apply();
+	},
+	mapping : function(){
+		//console.log(this.screen.x);
+	},
+	display : function(){
+		let s = '';
+		for(let i = this.map.length -1; i >= 0; i--){
+			for(let j = this.screen.x; j < this.screen.x + this.screen.large; j++){
+				s += ' ';
+				s += this.map[i][j];
+				s += ' ';
+				if(i == this.p.y && j == this.p.x){
+					s = s.slice(0, -2);
+					s += 5;
+					s += ' ';
+				}
+			}
+			s += '<br>';
+		}
+		this.canvas.innerHTML = s;
 	}
-	div.innerHTML = s;
 };
-display(map);
+g.init();
